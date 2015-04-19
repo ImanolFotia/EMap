0a17095252574a190cd00d573e13d4589f0e319f
ImanolFotia <imanolfotia@gmail.com>
2015-04-19 00:40:15 -0300

    Updated Map Parser

--- src/RawParser.cpp
+++ src/RawParser.cpp
@@ -42,40 +42,80 @@ EMap::EMap(const char* MapName)
     {
         eMapNumMeshes = eMap.mNumMeshes;
         Meshes.reserve(eMapNumMeshes);
-    }
 
-
-
-    for(int i = 0; i < eMapNumMeshes ; i++)
-    {
-
-        const struct aiMesh* mesh = eMap->mMeshes[i];
-
-        Mesh* tmpMesh = new Mesh();
-
-        tmpMesh->mNumVertices = mesh->mNumVertices;
-
-        for(int nVert = 0 ; nVert < mesh->mNumVertices; i++)
+        for(int i = 0; i < eMapNumMeshes ; i++)
         {
-            aiVector3D pos = mesh.mVertices[nVert];
-            VECTOR3D tmpvec3;
-            tmpvec3.x = pos.x;
-            tmpvec3.y = pos.y;
-            tmpvec3.z = pos.z;
-            tmpMesh->vPosition.push_back(tmpvec3);
-        }
 
-        if(mesh->HasTextureCoords())
-        {
+            const struct aiMesh* mesh = eMap->mMeshes[i];
+
+            Mesh* tmpMesh = new Mesh();
+
+            tmpMesh->mNumVertices = mesh->mNumVertices;
+
             for(int nVert = 0 ; nVert < mesh->mNumVertices; i++)
             {
-                aiVector2D txc = mesh.mTextureCoords[nVert];
-                VECTOR2D tmpvec2;
-                tmpvec2.x = pos.x;
-                tmpvec2.y = pos.y;
-                tmpMesh->TexCoords.push_back(tmpvec2);
+                aiVector3D pos = mesh.mVertices[nVert];
+                VECTOR3D tmpvec3;
+                tmpvec3.x = pos.x;
+                tmpvec3.y = pos.y;
+                tmpvec3.z = pos.z;
+                tmpMesh->vPosition.push_back(tmpvec3);
             }
+
+            if(mesh->HasTextureCoords())
+            {
+                for(int nVert = 0 ; nVert < mesh->mNumVertices; i++)
+                {
+                    aiVector2D txc = mesh.mTextureCoords[nVert];
+                    VECTOR2D tmpvec2;
+                    tmpvec2.x = txc.x;
+                    tmpvec2.y = txc.y;
+                    tmpMesh->TexCoords.push_back(tmpvec2);
+                }
+            }
+
+            if(mesh->HasNormals())
+            {
+                for(int nVert = 0 ; nVert < mesh->mNumVertices; i++)
+                {
+                    aiVector2D nor = mesh.mNormals[nVert];
+                    VECTOR3D tmpvec3;
+                    tmpvec3.x = nor.x;
+                    tmpvec3.y = nor.y;
+                    tmpvec3.z = nor.z;
+                    tmpMesh->vNormals.push_back(tmpvec3);
+                }
+            }
+
+            if(mesh->HasTangentsAndBitangents())
+            {
+                for(int nVert = 0 ; nVert < mesh->mNumVertices; i++)
+                {
+                    aiVector2D tan = mesh.mTangents[nVert];
+                    aiVector2D btan = mesh.mBitangents[nVert];
+                    VECTOR3D tmpvec3;
+                    tmpvec3.x = tan.x;
+                    tmpvec3.y = tan.y;
+                    tmpvec3.z = tan.z;
+                    tmpMesh->vTangents.push_back(tmpvec3);
+                    tmpvec3.x = btan.x;
+                    tmpvec3.y = btan.y;
+                    tmpvec3.z = btan.z;
+                    tmpMesh->vBitangents.push_back(tmpvec3);
+                }
+            }
+            eMap
+
+            delete tmpMesh;
         }
+        if(eMap->HasTextures())
+        const aiTexture* mtex = eMap->mTextures[0];
+
+        mtex->
+
+        const aiMaterial* mmat = eMap->mMaterials[0];
+
+        mmat->
 
 
     }
